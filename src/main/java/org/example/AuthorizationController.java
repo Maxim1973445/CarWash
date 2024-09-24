c8eb34d4db3776d4281aea25f85e8e67b0ce8013
Максим Майоров <maxim1973445@gmail.com>
2024-09-24 15:31:39 +0300

    Добалвены время открытия и закрытия в модлаьные окна, изменена карточка заказа, добавлены поля в модальное окно редактирова профиль

---
+++ src/main/java/org/example/AuthorizationController.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthorizationController {
+
+    // Метод для отображения главной страницы
+    @GetMapping("/")
+    public String mainPage(Model model) {
+        return "main"; // Возвращает шаблон авторизации
+    }
+
+
+    // Метод для отображения страницы авторизации
+    @GetMapping("/authorization")
+    public String authorization(Model model) {
+        return "authorization"; // Возвращает шаблон авторизации
+    }
+
+    // Метод для отображения страницы регистрации владельца автомойки
+    @GetMapping("/registrationowner")
+    public String registrationOwner(Model model) {
+        return "registrationowner"; // Возвращает шаблон регистрации владельца автомойки
+    }
+
+    // Метод для отображения стандартной страницы регистрации
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        return "registration"; // Возвращает стандартную страницу регистрации
+
+    }
+
+    // Метод для отображения стандартной страницы регистрации
+    @GetMapping("/owneraccount")
+    public String owneraccount(Model model) {
+        return "owneraccount"; // Возвращает стандартную страницу регистрации
+
+    }
+
+    @GetMapping("/clientaccount")
+    public String clientaccount(Model model) {
+        return "clientaccount"; // Возвращает стандартную страницу регистрации
+
+    }
+}
