c6a46ab8c8a4a7820b5f8574d25b9b30d51ae406
Илья Максименко <ilamaksimenko91@gmail.com>
2024-09-24 11:47:29 +0500

    Исправления ЛК, удаление пола в регестрациях, сдела переход на страницу stationList

---
+++ src/main/java/org/example/controllers/StationController.java
@@ -0,0 +1,22 @@
+package org.example.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.example.service.Impl.StationServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StationController {
+
+    @Autowired
+    private StationServiceImpl stationService;
+
+    @GetMapping(value = "/stationlist")
+    public String getStationList(Model model, HttpServletRequest request) {
+
+        model.addAttribute("station", stationService.getStationById(Integer.parseInt(request.getParameter("id"))));
+        return "stationList";
+    }
+}
