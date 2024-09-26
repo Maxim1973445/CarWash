be915c945cbb8acd4c84972070ffe2e6e751a377
Rustam P <russtampv@yandex.ru>
2024-09-26 14:40:43 +0500

    переделал получение статистики

--- src/main/java/org/example/controllers/StatisticController.java
+++ src/main/java/org/example/controllers/StatisticController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 
@@ -25,10 +26,15 @@ public class StatisticController {
 
     @PostMapping(value = "/ordersCount")
     public String getOrdersCount(@RequestParam("stationId") String stationId, Model model){
-        HashMap<String,Long> map = new HashMap<>();
         Long statId = Long.parseLong(stationId);
         List<Order> orders = orderService.getOrdersByStationId(statId);
-        map.put("all", (long) orders.size());
+        StatisticCalculate(orders, model);
+        model.addAttribute("stationId", statId);
+        return "statistic";
+    }
+
+    private void StatisticCalculate(List<Order> orders, Model model) {
+
         long cancelledOrdersCount = orders.stream().filter(order->order.getOrderStatus().equals(OrderStatus.CANCELLED)).count();
         long openedOrdersCount = orders.stream().filter(order->order.getOrderStatus().equals(OrderStatus.OPEN)).count();
         long inProgressOrdersCount = orders.stream().filter(order->order.getOrderStatus().equals(OrderStatus.INPROGRESS)).count();
@@ -41,12 +47,45 @@ public class StatisticController {
         long ordersPerMonth = orders.stream()
                 .filter(order->order.getStartTime().getYear()==year&&order.getStartTime().getMonthValue()==month)
                 .count();
+        model.addAttribute("all", (long) orders.size());
         model.addAttribute("cancelledOrders", cancelledOrdersCount);
         model.addAttribute("inProgressOrders", inProgressOrdersCount);
         model.addAttribute("completedOrders", completedOrdersCount);
         model.addAttribute("openedOrders", openedOrdersCount);
         model.addAttribute("uniqueClients", uniqueClientsCount);
         model.addAttribute("ordersPerMonth", ordersPerMonth);
+    }
+
+    @PostMapping(value = "/ordersByStationBetween")
+    public String getClientsStatistic(@RequestParam("stationId") String stationId,
+                                        @RequestParam("startDate") String startDate,
+                                      @RequestParam("endDate") String endDate,
+                                      Model model) {
+        model.addAttribute("defaultStartDate", startDate);
+        model.addAttribute("defaultEndDate", endDate);
+        if (stationId == null) {
+            return "statistic";
+        }
+        long statId = Long.parseLong(stationId);
+        LocalDate startDateTime = LocalDate.parse(startDate);
+        LocalDate endDateTime = LocalDate.parse(endDate);
+        if (startDateTime.isAfter(endDateTime)) {
+            model.addAttribute("message", "Дата начала выбора не может быть позже даты окончания!");
+            return "statistic";
+        }
+        LocalDateTime startTime = LocalDateTime.of(startDateTime.getYear(),
+                startDateTime.getMonth(),
+                startDateTime.getDayOfMonth(),
+                0, 0, 0);
+        LocalDateTime endTime = LocalDateTime.of(
+                endDateTime.getYear(),
+                endDateTime.getMonth(),
+                endDateTime.getDayOfMonth(),
+                23, 59, 59);
+
+        List<Order> orders = orderService.getOrdersByStationBetween(statId, startTime, endTime);
+        StatisticCalculate(orders, model);
+        model.addAttribute("stationId", statId);
         return "statistic";
     }
 }
